8249a2cb84ddd5d03a4f7ef7fc8a1230df5ade2a
Michael DeWitt <me.dewitt.jr@gmail.com>
2024-09-25 14:18:05 -0400

    fix the so

---
+++ src/PrimaryCensoredBase.jl
@@ -0,0 +1,29 @@
+@doc raw"
+A struct that contains the two different distributions, d1 and d2
+
+"
+
+struct PrimaryCensored{T<:Distributions.VariateForm,
+    S<:Distributions.ValueSupport} <:Distributions.Sampleable{T, S}
+    d1::Distributions.Sampleable{T,S}
+    d2::Distributions.Sampleable{T,S}
+end
+
+#! TODO clean up outside constructor
+PrimaryCensored(
+                d1::Distributions.Sampleable{S, T},
+                d2::Distributions.Sampleable{S, T}) where 
+                {S<:Distributions.VariateForm, T<:Distributions.ValueSupport} = 
+                PrimaryCensored{S,T}(d1,d2)
+
+# Method for single value
+function Base.rand(pc::PrimaryCensored)
+    rand(pc.d1) .+ rand(pc.d2)
+end
+
+# Method for multiple samples
+function Base.rand(pc::PrimaryCensored, n::Int)
+    d1_samps = rand(pc.d1, n)
+    d2_samps = rand(pc.d2, n)
+    return d1_samps + d2_samps
+end
