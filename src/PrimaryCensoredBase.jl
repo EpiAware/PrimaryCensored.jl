e91ee4fd7ef7a16e293967a0bfbd5e96aa38aa5b
Michael DeWitt <me.dewitt.jr@gmail.com>
2024-09-25 14:27:07 -0400

    please work

--- src/PrimaryCensoredBase.jl
+++ src/PrimaryCensoredBase.jl
@@ -3,7 +3,9 @@ A struct that contains the two different distributions, d1 and d2
 
 "
 
-struct PrimaryCensored{T<:Distributions.VariateForm,
+
+
+struct PrimaryCensoredBeta{T<:Distributions.VariateForm,
     S<:Distributions.ValueSupport} <:Distributions.Sampleable{T, S}
     d1::Distributions.Sampleable{T,S}
     d2::Distributions.Sampleable{T,S}
@@ -27,3 +29,5 @@ function Base.rand(pc::PrimaryCensored, n::Int)
     d2_samps = rand(pc.d2, n)
     return d1_samps + d2_samps
 end
+                PrimaryCensoredBeta{S,T}(d1,d2)
+
--- src/primarycensored.jl
+++ src/primarycensored.jl
@@ -2,7 +2,11 @@ module primarycensored
 
 # Non-submodule imports
 using DocStringExtensions
+using Distributions
 
 include("docstrings.jl")
 
+include("PrimaryCensoredBase.jl")
+
 end
+
