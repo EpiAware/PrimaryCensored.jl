ee27d9a135c91d535b2ebe4ccfee6a3e68e7a068
Michael DeWitt <me.dewitt.jr@gmail.com>
2024-09-26 09:50:00 -0400

    WithinIntervalCensored crude approach
    * Creates a struct
    * Constructor
    * method to generate the pmf

--- src/WithinIntervalCensored.jl
+++ src/WithinIntervalCensored.jl
@@ -11,34 +11,65 @@ for the cumulative distribution function.
 - `pwindow` primary censoring window
 
 # Returns
-- WithinIntervalCensor object
+- WithinIntervalCensoredDist object
 
 "
 #! TODO Struct
-struct WithinIntervalCensor
-
+struct WithinIntervalCensoredDist{D <: UnivariateDistribution, OBS <: Real, SWIN <:Real} <:
+    Distributions.UnivariateDistribution{Distributions.ValueSupport}
+    dist::D
+    obs_time::OBS
+    swindow::SWIN
 end
 
+@doc raw"
+Generic wrapper for within window censored an object
+
+# Contructors
+
+"
+function withinintervalcensoreddist(dist::UnivariateDistribution, obs_time::Real, swindow::Real)
+    WithinIntervalCensoredDist(dist, obs_time, swindow)
+end
 
 #! Method of evaluation
 #! Gives full discretized interval censored pmf for some distribution
 #! Note the -1 to represent indexing from zero
 
-function within_interval_censored(dist, obs_time, swindow::Real)
-    if obs_time < 0
-        throw(error("`obs_time` = $obs_time and should not be negative"))
+@doc raw"
+Returning a within interval censored pmf
+
+# Arguments
+- d an WithinIntervalCensoredDist object
+
+# Returns
+- discretized probability mass function
+
+# Examples
+
+```@example
+    using PrimaryCensored, Distributions
+    d= truncated(Normal(5,2),0,5)
+    trunc_d = withinintervalcensoreddist(d, 4, 1.0)
+    within_interval_censored(trunc_d)
+```
+"
+
+function within_interval_censored(d::WithinIntervalCensoredDist)
+    dobs = d.obs_time
+    if d.obs_time < 0
+        throw(error("`obs_time` = $dobs and should not be negative"))
     end
 
-    out = zeros(obs_time)
+    obs_time_integer = floor(Int, dobs)
+
+    out = zeros(obs_time_integer)
     for n in eachindex(out)
-        out[n] = cdf(dist, n + swindow - 1) - cdf(dist, n - 1)
+        out[n] = cdf(d.dist, n + d.swindow - 1) - cdf(d.dist, n - 1)
     end
     # Normalise
     out = out ./ sum(out)
 
     return out
 end
-
-
-
 
