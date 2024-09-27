34db950983aa73b3d508d69c70b214f62e7101f0
Michael DeWitt <me.dewitt.jr@gmail.com>
2024-09-27 15:44:18 -0400

    Maybe this will make it happy?
    Unsure how the TestItems syntax is treating these things.

--- test/PrimaryCensoredDist.jl
+++ test/PrimaryCensoredDist.jl
@@ -7,27 +7,28 @@ function generate_object()
     primarycensored(LogNormal(3.5, 1.5), Uniform(1,2))
 end
 
+use_dist = generate_object()
+
 # Bad inputs
-@testitem "struct" begin
+@testitem "Test struct type" begin
     @test_throws MethodError primarycensored(Normal(1,2), 7)
 end
 
-
 @testitem "Test constructor" begin
-    @test typeof(generate_object()) <: PrimaryCensored.PrimaryCensoredDist
+    @test typeof(use_dist) <: PrimaryCensored.PrimaryCensoredDist
 end
 
 @testitem "Test random generation" begin
-    @test length(rand(generate_object(),10)) == 10
+    @test length(rand(use_dist,10)) == 10
 end
 
 @testitem  "Test cdf method" begin
-    @test cdf(generate_object(),1e8) ≈ 1.0
+    @test cdf(use_dist,1e8) ≈ 1.0
 end
 
 
 @testitem "Test ccdf" begin
-    @test ccdf(generate_object(),1e8) ≈ 0.0
-    @test ccdf(generate_object(), 0.0) ≈ 1
+    @test ccdf(use_dist,1e8) ≈ 0.0
+    @test ccdf(use_dist, 0.0) ≈ 1
 end
 
